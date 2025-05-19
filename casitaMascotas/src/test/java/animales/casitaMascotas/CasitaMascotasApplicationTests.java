b45925eb396ce214b3b36a2ce2134e6928756dd1
Alam <alamblanco18@gmail.com>
2025-05-19 14:50:28 -0300

    PrimerOSegundoCommit

--- casitaMascotas/src/test/java/animales/casitaMascotas/CasitaMascotasApplicationTests.java
+++ casitaMascotas/src/test/java/animales/casitaMascotas/CasitaMascotasApplicationTests.java
@@ -2,8 +2,15 @@ package animales.casitaMascotas;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+		"spring.datasource.url=jdbc:mysql://localhost:3306/la_casita",
+		"spring.datasource.username=root",
+		"spring.datasource.password="
+})
+
 class CasitaMascotasApplicationTests {
 
 	@Test
